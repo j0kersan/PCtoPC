9afa1d6935bc479c502241f3ca0bfd6ff2264068
Samuel <miquelspro@gmail.com>
2021-10-28 18:43:07 -0400

    Power Reset Added

--- Proto101_MessyMind/Assets/Scripts/CharacterControls.cs
+++ Proto101_MessyMind/Assets/Scripts/CharacterControls.cs
@@ -11,17 +11,24 @@ public class CharacterControls : MonoBehaviour
     public bool keyLeft;
     public bool keyRight;
     public bool powerAct;
+    public bool powerUse;
 
+    //Nombres de pas du joueurs
     public int charaStep;
 
     // Update is called once per frame
+    private void Start()
+    {
+        powerUse = true;
+    }
     void Update()
     {
 
         keyRight = false;
         keyLeft = false;
 
-        
+
+        //Conditions d'utilisation des mouvements
         #region CharaMoves
 
         if (Input.GetKeyDown("left") && moveCond.onTheMove == false)
@@ -39,13 +46,13 @@ public class CharacterControls : MonoBehaviour
             charaStep += 1;
         }
 
-
-
         #endregion
 
+
+        //Conditions d'utilisation des pouvoirs du personnage
         #region CharaPowers
 
-        if (Input.GetKeyDown("f"))
+        if (Input.GetKeyDown("f") && powerUse == true)
         {          
             StartCoroutine(powerStart());
         }
@@ -53,6 +60,7 @@ public class CharacterControls : MonoBehaviour
         IEnumerator powerStart()
         {
             powerAct = true;
+            powerUse = false;
             charaStep = 0;
             Debug.Log("Coroutine lancé");
 
@@ -62,11 +70,23 @@ public class CharacterControls : MonoBehaviour
                 {
                     powerAct = false;
                     Debug.Log("Pouvoir arrété");
-                    yield break;
-                }
 
-            Debug.Log("Stop Coroutine");
+                yield return StartCoroutine(powerReset());
+                }
             
+        }
+
+        IEnumerator powerReset()
+        {
+            Debug.Log("Reset en cours");
+            yield return new WaitUntil(() => charaStep == 11);
+
+            if (charaStep == 11)
+            {
+                powerUse = true;
+                Debug.Log("Pouvoir reset");
+                yield break;
+            }
         }
         #endregion
     }
